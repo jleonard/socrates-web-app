9b727615326e718cc1099607c3576e196789a0b7
John Leonard <thejohnleonard@gmail.com>
2025-02-09 16:35:26 -0500

    fix the border

--- app/routes/app/components/agent-waiting.tsx
+++ app/routes/app/components/agent-waiting.tsx
@@ -1,7 +1,7 @@
 const AgentWaiting = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen">
-      <div className="w-20 h-20 border-6 border-black border-t-white rounded-full animate-spin"></div>
+      <div className="w-20 h-20 border border-7 border-black border-t-white rounded-full animate-spin"></div>
     </div>
   );
 };
--- app/routes/app/web-stt.tsx
+++ app/routes/app/web-stt.tsx
@@ -117,7 +117,7 @@ const ParentComponent: React.FC = () => {
       </div>
       <div className="flex flex-col items-center justify-center w-screen h-screen">
         <SpeechRecognitionButton className="" onTranscript={handleTranscript} />
-        {isSpeaking && <VoiceBars />}
+        {!isSpeaking && <VoiceBars />}
         {!isWaiting && <AgentWaiting />}
         <WavPlayer className="fixed bottom-4 right-4 opacity-0" url={curUrl} />
       </div>
