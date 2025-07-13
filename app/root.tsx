826f095ca5a77cd3f17a3b574599e41e71d6a3d0
John Leonard <thejohnleonard@gmail.com>
2025-07-13 15:32:47 -0400

    position the history button

--- app/root.tsx
+++ app/root.tsx
@@ -65,7 +65,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           </>
         )}
       </head>
-      <body className="px-8 max-w-[1024px] mx-auto">
+      <body className="px-8 max-w-[1024px] mx-auto relative min-h-screen">
         <Nav />
         {children}
         <ScrollRestoration />
