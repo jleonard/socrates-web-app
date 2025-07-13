317395bb6ecfcd3e02aacef4a8fd77dac1b4fe56
John Leonard <thejohnleonard@gmail.com>
2025-07-12 21:05:25 -0400

    alert design

--- components/ErrorMessage/ErrorMessage.tsx
+++ components/ErrorMessage/ErrorMessage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { CircleAlert } from "lucide-react";
 
 type ErrorMessageProps = {
   message: string;
@@ -20,8 +21,8 @@ export function ErrorMessage({ message, onClear }: ErrorMessageProps) {
   if (!visible) return null;
 
   return (
-    <div className=" bg-red-200 text-red-800 px-4 py-2 rounded shadow-md transition-opacity duration-500">
-      {message}
+    <div className=" bg-red-100 text-red-800 px-4 py-2 rounded shadow-md transition-opacity duration-500 flex flex-row gap-2 align-middle">
+      <CircleAlert className="text-red-800" /> {message}
     </div>
   );
 }
