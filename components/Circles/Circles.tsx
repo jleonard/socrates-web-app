665b7836b1661fe911e5588341933e770fdc9748
John Leonard <thejohnleonard@gmail.com>
2025-08-02 19:43:23 -0400

    circle

--- components/Circles/Circles.tsx
+++ components/Circles/Circles.tsx
@@ -206,7 +206,7 @@ export const Circles = forwardRef<HTMLDivElement, CirclesProps>(
             variants={{
               processing: {
                 rotate: 360,
-                y: -85, // moved up from 0
+                y: -88, // moved up from 0
                 scale: 0.99,
                 transition: {
                   rotate: {
