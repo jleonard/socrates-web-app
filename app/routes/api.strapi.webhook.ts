f3f69662e33bcb2a3e5f7421eb6a816d7c371c7c
John Leonard <thejohnleonard@gmail.com>
2026-09-21 20:44:52 -0400

    adding name to db

--- app/routes/api.strapi.webhook.ts
+++ app/routes/api.strapi.webhook.ts
@@ -721,6 +721,9 @@ async function buildLocationRelationship(entry: Record<string, any>) {
     if (entry?.card?.thumbnail) {
       payload.thumbnail = entry.card.thumbnail.url;
     }
+    if (entry?.name) {
+      payload.name = entry.name;
+    }
 
     const { error } = await supabaseAdmin
       .from("location_relationships")
